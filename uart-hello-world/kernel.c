a2347b72e9b12fe245c77e1732dcb48b667e0958
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2023-07-25 21:13:07 +0530

    changes

--- uart-hello-world/kernel.c
+++ uart-hello-world/kernel.c
@@ -2,7 +2,20 @@
 
 void main()
 {
+    int basic_arr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     uart_init();
-    uart_writeText("Hello world!\n");
-    while (1);
+    uart_writeText("Hello World \n");
+    while (1)
+    {
+        {
+            volatile unsigned long long i;
+            for (i = 0; i < 1000000000ULL; ++i)
+            {
+                for (int j = 0; j < 72; j++)
+                {
+                    basic_arr[j] += 1;
+                }
+            }
+        }
+    }
 }
