1f8f0a57e280edd346ffac95a40d140fc325cf2c
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2024-01-05 12:00:24 -0500

    adding files

---
+++ rust-os-doccumented/target/thumbv7m-none-eabi/debug/deps/rust_os_doccumented-6c9e84143efffac6.d
@@ -0,0 +1,6 @@
+/home/nandu/honors-rpi4os/rust-os-doccumented/target/thumbv7m-none-eabi/debug/deps/librust_os_doccumented-6c9e84143efffac6.rmeta: src/main.rs src/io.rs
+
+/home/nandu/honors-rpi4os/rust-os-doccumented/target/thumbv7m-none-eabi/debug/deps/rust_os_doccumented-6c9e84143efffac6.d: src/main.rs src/io.rs
+
+src/main.rs:
+src/io.rs:
